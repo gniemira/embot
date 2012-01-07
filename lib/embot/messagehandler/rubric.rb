33429df2a3c20460bc864826df5960b2f209e1e4
Gerard Niemira <gniemira@gmail.com>
2012-01-06 16:51:58 -0800

    more rubric entries

--- lib/embot/messagehandler/rubric.rb
+++ lib/embot/messagehandler/rubric.rb
@@ -1,12 +1,6 @@
 module Embot
   module MessageHandler
 
-    # Quote message handler
-    #
-    # This message handler will post a random quote from
-    # www.quotationspage.com
-    #
-    # Triggers on command: embot quote
     class Rubric < Base
       def process(message)
         return nil if !message.is_for_embot? || !message.command_is?('rubric')
@@ -16,7 +10,7 @@ module Embot
       private
 
       def nameify  
-        rubric_list = ["rocket", "laser",	"dinosaur",	"old people",	"tiger", "chicken",	"steel",	"crystal",	"engine",	"lady",	"baby",	"rhino",	"panda",	"apocalypse",	"zombie",	"ninja",	"harrison ford",	"space",	"underpants",	"future",	"robot",	"chocolate",	"shark",	"jet",	"gun",	"guitar solo",	"beard",	"sparkle",	"fire",	"monkey",	"cyber",	"fist",	"unicorn",	"mustache", "juice"]
+        rubric_list = ["rocket", "laser",	"dinosaur",	"old people",	"tiger", "chicken",	"steel",	"crystal",	"engine",	"lady",	"baby",	"rhino",	"panda",	"apocalypse",	"zombie",	"ninja",	"harrison ford",	"space",	"underpants",	"future",	"robot",	"chocolate",	"shark",	"jet",	"gun",	"guitar solo",	"beard",	"sparkle",	"fire",	"monkey",	"cyber",	"fist",	"unicorn",	"mustache", "juice", "winning", "charlie sheen", "rapture", "torture", "spasm", "bro", "manzierre", "lady gaga", "worm", "Premal's phone", "jedi", "toupee", "botox", "cheeseburger", "Coolio", "Tom Brady", "bone", "Poland", "George W. Bush", "satin", "butter", "muffin", "milkshake", "cream pie", "sword", "giant", "queen", "bear", "rainbow", "tornado", "lips", "carrot", "donut", "fraternity", "Salesforce", "trophy", "penguin", "Jon Kart", "shake weight", "snuggie", "Goulet"]
         name = rubric_list.sample + " " + rubric_list.sample
         return name
       end
